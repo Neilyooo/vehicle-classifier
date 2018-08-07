6ade4f1226313bb5327effcf7203a8efa6b5e213
Neilyooo <39515003+Neilyooo@users.noreply.github.com>
2018-08-08 02:20:36 +0800

    Update dataset_factory.py

--- datasets/dataset_factory.py
+++ datasets/dataset_factory.py
@@ -22,7 +22,7 @@ from datasets import cifar10
 from datasets import flowers
 from datasets import imagenet
 from datasets import mnist
-
+from datasets import vehicle
 datasets_map = {
     'cifar10': cifar10,
     'flowers': flowers,
